0bfa64f9e85108121c87d4991f53075cad5d486e
EzelHong <125138510+EzelHong@users.noreply.github.com>
2023-05-31 03:15:16 +0800

    Update FA_JPFood.php

--- PHP Final Assignment/FA_JPFood.php
+++ PHP Final Assignment/FA_JPFood.php
@@ -4,7 +4,6 @@
     $dbpassword = "ROOT28";
     $dbname = "Final";
 
-    // Create a connection
     $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
     // Check the connection
@@ -15,7 +14,7 @@
     // Select the database
     $conn->select_db($dbname);
 
-    // Create the "food" table if it doesn't exist
+    // Create the "JPFood" table if it doesn't exist
     $query = "CREATE TABLE IF NOT EXISTS JPFood (
                 FoodID INT(10) PRIMARY KEY AUTO_INCREMENT,
                 FoodName VARCHAR(50) NOT NULL,
@@ -155,7 +154,8 @@
             echo "<td>" . $foodName . "</td>";
             echo "<td><img src='images/JF$i.jpg' height='120' width='120'/></td>";
             echo "<td>" . $foodPrice . "</td>";
-            echo "<td><a class='buy-now-btn' href='FA_Cart.php?id=" . $foodID . "&name=" . urlencode($foodName) . "&price=" . $foodPrice . "'>Buy Now</a></td>";
+            echo "<td><a class='buy-now-btn' href='FA_Cart.php?id=" . $foodID . "&name=" . urlencode($foodName) 
+            . "&price=" . $foodPrice . "'>Buy Now</a></td>";
             echo "</tr>";
             $i++;
         }
