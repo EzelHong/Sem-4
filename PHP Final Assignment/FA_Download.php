1f005d5c4c82fdc42f7e3ed58e7bd0366aff2cde
EzelHong <125138510+EzelHong@users.noreply.github.com>
2023-06-12 13:42:59 +0800

    Update FA_Download.php

--- PHP Final Assignment/FA_Download.php
+++ PHP Final Assignment/FA_Download.php
@@ -1,8 +1,7 @@
 <?php
     session_start();
-
     $name = $_SESSION['username'];
-
+    
     $servername = "localhost";
     $dbusername = "root";
     $dbpassword = "ROOT28";
@@ -14,37 +13,34 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Retrieve the username and address from the user table
     $userQuery = "SELECT Username, Email, Address FROM users WHERE Username = '$name'";
     $userResult = mysqli_query($conn, $userQuery);
 
     if (mysqli_num_rows($userResult) > 0) {
-        // Fetch the username and address
+        // Fetch the username, email, and address
         $userData = mysqli_fetch_assoc($userResult);
         $username = $userData['Username'];
         $email = $userData['Email'];
         $address = $userData['Address'];
 
-        // Retrieve all rows from the table
         $query = "SELECT * FROM $name";
         $result = mysqli_query($conn, $query);
 
         if (mysqli_num_rows($result) > 0) {
-            // Define the filename for the download
             $filename = "Cart_data.csv";
 
             // Set the appropriate headers for CSV download
             header('Content-Type: text/csv');
             header('Content-Disposition: attachment; filename="' . $filename . '"');
 
-            // Open a file pointer for writing
+            // Open file pointer for writing
             $fp = fopen('php://output', 'w');
 
-            // Write the username and address as CSV headers
+            // Write the username, email, and address as CSV headers
             fputcsv($fp, array('Username', 'Email', 'Address'));
             fputcsv($fp, array($username, $email, $address));
 
-            // Get the column names
+            // Get column names
             $columns = mysqli_fetch_fields($result);
             $columnNames = array();
             foreach ($columns as $column) {
