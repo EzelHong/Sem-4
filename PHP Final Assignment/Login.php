b9222c7747d547a1d434f6df5a831b72461564e1
EzelHong <125138510+EzelHong@users.noreply.github.com>
2023-06-12 13:46:41 +0800

    Update Login.php

--- PHP Final Assignment/Login.php
+++ PHP Final Assignment/Login.php
@@ -2,10 +2,9 @@
     session_start();
 
     if (isset($_POST["submit"])) {
-        // Retrieve user inputs
         $name = $_POST["username"];
         $userpassword = $_POST["password"];
-
+        
         $servername = "localhost";
         $dbusername = "root";
         $dbpassword = "ROOT28";
@@ -17,16 +16,13 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        // Check username and password match
         $sql = "SELECT * FROM users WHERE username = '$name' AND password = '$userpassword'";
         $result = $conn->query($sql);
 
         if ($result->num_rows == 1) {
-            // Login successful take session username
             echo "Login successful";
             $_SESSION['username'] = $name;
 
-            // Create a cart table for the user if it doesn't exist
             $createCartTableQuery = "CREATE TABLE IF NOT EXISTS $name (
                 CartID INT(10) PRIMARY KEY AUTO_INCREMENT,
                 FoodID INT(10) NOT NULL,
